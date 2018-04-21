32a74216f0e487dd5665cdae6a76a29d4ca43401
tzar <tzar@tzar>
2018-04-21 13:01:06 -0500

    TZAR COMMIT ASOCIADOS

--- application/models/AsociadosModel.class.php
+++ application/models/AsociadosModel.class.php
@@ -9,7 +9,7 @@
             $sql = "SELECT CODIGO_ASOCIADO FROM $this->table ORDER BY CODIGO_ASOCIADO DESC";
             $result = $this->db->getOne($sql);
             $sql = "INSERT INTO $this->table VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            $this->db->query($sql, $result + 1, $params['nombre'], $params['apellidoP'], $params['apellidoM'], $params['nacimiento'], $params['edad'], $params['sexo'], 1, $params['rfc'], $params['curp'], $params['lugarNacimiento'], $params['nacionalidad'], $params['estadoCivil'], 1, $params['correo'], $params['telefono'], $params['domicilio'], $params['colonia'], $params['delegacion'], $params['ciudad'], $params['estado'],  $params['entreCalles'], "jajaa", $params['cp'], $params['domicilioFiscal'], $params['cpFiscal'], $params['fechaInicio'], $params['fechaBaja'], $params['desarrollo'], $params['puesto']);
+            $this->db->query($sql, $result + 1, $params['nombre'], $params['apellidoP'], $params['apellidoM'], $params['nacimiento'], $params['edad'], $params['sexo'], $params['hijos'], $params['rfc'], $params['curp'], $params['lugarNacimiento'], $params['nacionalidad'], $params['estadoCivil'], $params['extranjero'], $params['correo'], $params['telefono'], $params['domicilio'], $params['colonia'], $params['delegacion'], $params['ciudad'], $params['estado'], $params['entreCalle1'], $params['entreCalle2'], $params['cp'], $params['domicilioFiscal'], $params['cpFiscal'], $params['fechaInicio'], $params['fechaBaja'], $params['desarrollo'], $params['puesto']);
             $result = $this->db->getAll($this->table);
             return $result;
     	}//insertAsociados
@@ -21,18 +21,18 @@
             return $result;
     	}//deleteGiros
     
-    	public function updateGiros($id, $nombre){
-            $sql = "UPDATE $this->table SET NOMBRE = ? WHERE CODIGO_GIRO = ?";
-            $this->db->query($sql, $nombre, $id);
+    	public function updateAsociados($params){
+            $sql = "UPDATE $this->table SET NOMBRE = ?, APELLIDO_P = ?, APELLIDO_M = ?, FECHA_NACIMIENTO = ?, EDAD = ?, SEXO = ?, HIJOS = ?, RFC = ?, CURP = ?, LUGAR_NACIMIENTO = ?, NACIONALIDAD = ?, ESTADO_CIVIL = ?, EXTRAJERO = ?, EMAIL = ?, TELEFONO = ?, DOMICILIO = ?, COLONIA = ?, DELEGACION = ?, CIUDAD = ?, ESTADO = ?, ENTRE_CALLE_1 = ?, ENTRE_CALLE_2 = ?, CP = ?, DOMICILIO_FISCAL = ?, CP_FISCAL = ?, FECHA_INICIO = ?, FECHA_BAJA = ?, CODIGO_DESARROLLO = ?, CODIGO_PUESTO = ? WHERE CODIGO_ASOCIADO = ?";
+            $this->db->query($sql, $params['nombre'], $params['apellidoP'], $params['apellidoM'], $params['nacimiento'], $params['edad'], $params['sexo'], $params['hijos'], $params['rfc'], $params['curp'], $params['lugarNacimiento'], $params['nacionalidad'], $params['estadoCivil'], $params['extranjero'], $params['correo'], $params['telefono'], $params['domicilio'], $params['colonia'], $params['delegacion'], $params['ciudad'], $params['estado'], $params['entreCalle1'], $params['entreCalle2'], $params['cp'], $params['domicilioFiscal'], $params['cpFiscal'], $params['fechaInicio'], $params['fechaBaja'], $params['desarrollo'], $params['puesto'], $params['id']);
             $result = $this->db->getAll($this->table);
             return $result;
     	}//updateUsuarios
         
-        public function retriveGiros($id){
-            $sql = "SELECT * FROM $this->table WHERE CODIGO_GIRO = ?";
+        public function retriveAsociados($id){
+            $sql = "SELECT * FROM $this->table WHERE CODIGO_ASOCIADO = ?";
             $result = $this->db->queryAllById($sql, $id);
             return $result;
-        }//retriveGiros
+        }//retriveAsociados
         
         public function getSelectedColumns(){
             $sql = "SELECT CODIGO_ASOCIADO, NOMBRE FROM $this->table";
