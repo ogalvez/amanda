bccefaa0e2dbf1a1b7919bbf419b37b49cba3448
tzar <tzar@tzar>
2018-04-24 09:32:23 -0500

    TZAR COMMIT ASOCIADOS MODEL

--- application/models/AsociadosModel.class.php
+++ application/models/AsociadosModel.class.php
@@ -10,8 +10,11 @@
             $result = $this->db->getOne($sql);
             $sql = "INSERT INTO $this->table VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $this->db->query($sql, $result + 1, $params['nombre'], $params['apellidoP'], $params['apellidoM'], $params['nacimiento'], $params['edad'], $params['sexo'], $params['hijos'], $params['rfc'], $params['curp'], $params['lugarNacimiento'], $params['nacionalidad'], $params['estadoCivil'], $params['extranjero'], $params['correo'], $params['telefono'], $params['domicilio'], $params['colonia'], $params['delegacion'], $params['ciudad'], $params['estado'], $params['entreCalle1'], $params['entreCalle2'], $params['cp'], $params['domicilioFiscal'], $params['cpFiscal'], $params['fechaInicio'], $params['fechaBaja'], $params['desarrollo'], $params['puesto']);
-            $result = $this->db->getAll($this->table);
-            return $result;
+            //$sql = "INSERT INTO IMSS VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+            //$this->db->query($sql, $params['numeroIMSS'], $params['FAIMSS'], $params['FBIMSS'], $params['sueldoIMSS'], $params['clinicaIMSS'], $params['tipoIMSS'], $params['comentarioIMSS'], $result + 1);
+            //$result = $this->db->getAll($this->table);
+            //return $result;
+            return $result + 1;
     	}//insertAsociados
     
     	public function deleteAsociados($id){
@@ -24,8 +27,8 @@
     	public function updateAsociados($params){
             $sql = "UPDATE $this->table SET NOMBRE = ?, APELLIDO_P = ?, APELLIDO_M = ?, FECHA_NACIMIENTO = ?, EDAD = ?, SEXO = ?, HIJOS = ?, RFC = ?, CURP = ?, LUGAR_NACIMIENTO = ?, NACIONALIDAD = ?, ESTADO_CIVIL = ?, EXTRAJERO = ?, EMAIL = ?, TELEFONO = ?, DOMICILIO = ?, COLONIA = ?, DELEGACION = ?, CIUDAD = ?, ESTADO = ?, ENTRE_CALLE_1 = ?, ENTRE_CALLE_2 = ?, CP = ?, DOMICILIO_FISCAL = ?, CP_FISCAL = ?, FECHA_INICIO = ?, FECHA_BAJA = ?, CODIGO_DESARROLLO = ?, CODIGO_PUESTO = ? WHERE CODIGO_ASOCIADO = ?";
             $this->db->query($sql, $params['nombre'], $params['apellidoP'], $params['apellidoM'], $params['nacimiento'], $params['edad'], $params['sexo'], $params['hijos'], $params['rfc'], $params['curp'], $params['lugarNacimiento'], $params['nacionalidad'], $params['estadoCivil'], $params['extranjero'], $params['correo'], $params['telefono'], $params['domicilio'], $params['colonia'], $params['delegacion'], $params['ciudad'], $params['estado'], $params['entreCalle1'], $params['entreCalle2'], $params['cp'], $params['domicilioFiscal'], $params['cpFiscal'], $params['fechaInicio'], $params['fechaBaja'], $params['desarrollo'], $params['puesto'], $params['id']);
-            $result = $this->db->getAll($this->table);
-            return $result;
+            //$result = $this->db->getAll($this->table);
+            //return $result;
     	}//updateUsuarios
         
         public function retriveAsociados($id){
